ff861734fb954e2801262cd256a084ffabe6bd94
articuly <articuly@gmail.com>
2019-12-21 11:26:45 +0800

    中文分词分析

---
+++ 中文分词分析pkuseg-python模块.py
@@ -0,0 +1,25 @@
+# coding:utf-8
+import pkuseg
+from collections import Counter
+import pprint
+
+content = []
+with open('2020.txt', encoding='utf-8') as f:
+    content = f.read()
+
+lexicon = ['老顾客', '沉寂顾客', '新购顾客']
+seg = pkuseg.pkuseg(user_dict=lexicon)  # 加载模型，给定用户词典
+text = seg.cut(content)
+
+stopwords = []
+with open('stopword.txt', encoding='utf-8') as f:
+    stopwords = f.read()
+
+new_text = []
+for w in text:
+    if w not in stopwords:
+        new_text.append(w)
+
+# print(new_text)
+counter = Counter(new_text)
+pprint.pprint(counter.most_common(100))

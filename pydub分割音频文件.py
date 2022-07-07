5ef989b419b60ff26c51804ac757e84c7b54400e
articuly <articuly@gmail.com>
2022-07-07 19:46:46 +0800

    update

--- pydub分割音频文件.py
+++ pydub分割音频文件.py
@@ -1,12 +1,12 @@
 from pydub import AudioSegment
 from pydub.utils import make_chunks
 
-myaudio = AudioSegment.from_file(r"E:\Downloads\tax_meeting.wav", "wav")
-chunk_length_ms = 1000 * 600  # 分块的毫秒数
+myaudio = AudioSegment.from_file(r"C:\Users\ASL\lcqqdwjdbjyj.aac", "aac")
+chunk_length_ms = 22000 * 600  # 分块的毫秒数
 chunks = make_chunks(myaudio, chunk_length_ms)  # 将文件切割成1秒每块
 
 # 保存切割的音频到文件
 for i, chunk in enumerate(chunks):
-    chunk_name = r".\audio\chunk{0:0>3d}.wav".format(i)
+    chunk_name = r".\audio\财经历史《两次全球大危机的比较研究》{0:0>2d}.aac".format(i)
     print("exporting...", chunk_name)
-    chunk.export(chunk_name, format="wav")
+    chunk.export(chunk_name, format="adts")
--- 批量重命名去后缀.py
+++ 批量重命名去后缀.py
@@ -2,13 +2,21 @@
 import os
 import re
 
-path = 'C:\\Users\\artic\Music\\Song'
+# path='C:\\Users\\artic\Music\\Song'
+path = r'E:\Documents\09.金融\BTC report'
 
 f = os.listdir(path)
 
 for i in f:
-    fname, ext = os.path.splitext(i)
-    oldname = path + '\\{}'.format(i)
+    # 替换名字的空格
+    new = i.replace(' ', '-')
+    os.chdir(path)
+    os.rename(i, new)
+
+    # fname, ext = os.path.splitext(i)
+    # oldname = path + '\\{}'.format(i)
+
+    # 修改特定后缀的名称
     # if re.match('.*?\[mqms2\]',i) or re.match('.*?\[weiyun\]', i):
     #     newname=path+'\\{0}{1}'.format(fname[:-8], ext)
     #     print(oldname, newname)
@@ -16,15 +24,19 @@ for i in f:
     #         os.renames(oldname, newname)
     #     except Exception as e:
     #         print(e)
+
+    # 删除名称后缀
     # if re.match('.*?\[mqms\]', i):
     #     newname = path + '\\{0}{1}'.format(fname[:-7], ext)
     #     try:
     #         os.renames(oldname, newname)
     #     except Exception as e:
     #         print(e)
-    if re.match('.*?&.*?', i):
-        fname = fname.replace('&', ',')
-        newname = path + '\\{0}{1}'.format(fname, ext)
-        print(oldname)
-        # os.remove(oldname)
-        # os.renames(oldname, newname)
+
+    # 修改包括特殊字符名称
+    # if re.match('.*?&.*?', i):
+    #     fname=fname.replace('&', ',')
+    #     newname=path+'\\{0}{1}'.format(fname, ext)
+    #     print(oldname)
+    #     os.remove(oldname)
+    #     os.renames(oldname, newname)
